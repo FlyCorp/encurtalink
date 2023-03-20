00585c028299b1acb71ad7e7e47f4abab3971e84
MichellHPC <56286200+MichellHPC@users.noreply.github.com>
2023-03-20 17:12:03 -0300

    form edit because google gtm

--- app/Http/Requests/PostUpdateShortUrl.php
+++ app/Http/Requests/PostUpdateShortUrl.php
@@ -14,11 +14,9 @@ class PostUpdateShortUrl extends FormRequest
 
     public function rules()
     {   
-        //dd($this->all());
+        
         return
             [
-                'id'          => 'required|exists:short_urls,id',
-                'code'        => ($this->code_original!=$this->code)?'required|unique:short_urls,code,NULL':"",
                 'link'        => 'required|url',
                 'description' => 'required',
             ];
