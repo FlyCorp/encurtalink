bdb4b483f7612a87fc620e5b0f1b3056656ef905
devpaulopaixao <paulinhohenrique90@yahoo.com.br>
2024-03-04 16:48:32 -0300

    Update config.nps.path logical

--- config/nps.php
+++ config/nps.php
@@ -1,5 +1,5 @@
 <?php
 
 return [
-    'path' => env('APP_NAME') == "Central Nutrition" ? "assets/media/nps/central-nutrition" : "assets/media/nps/central-farma"
+    'path' => env('APP_NAME') != "Central Nutrition" ? "assets/media/nps/central-farma" : "assets/media/nps/central-nutrition"
 ];
